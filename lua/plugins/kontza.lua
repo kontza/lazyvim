b7179531cf5225f61f2a26e913d3a79bbca20486
Juha Ruotsalainen <juha.ruotsalainen@ambientia.fi>
2023-12-18 12:55:09 +0200

    goimports added

--- lua/plugins/kontza.lua
+++ lua/plugins/kontza.lua
@@ -7,15 +7,26 @@ return {
     opts = {
       -- Define your formatters
       formatters_by_ft = {
+        arduino = { "clang_format" },
+        c = { "clang_format" },
+        cpp = { "clang_format" },
+        cs = { "clang_format" },
         css = { "prettier" },
+        cuda = { "clang_format" },
+        go = { "goimports" },
         graphql = { "prettier" },
         handlebars = { "prettier" },
         html = { "prettier" },
+        java = { "google_java_format" },
         javascript = { "prettier" },
         javascriptreact = { "prettier" },
         json = { "prettier" },
         jsonc = { "prettier" },
         less = { "prettier" },
+        lua = { "stylua" },
+        proto = { "clang_format" },
+        python = { "isort", "black" },
+        rust = { "rustfmt" },
         scss = { "prettier" },
         svelte = { "prettier" },
         toml = { "prettier" },
@@ -23,16 +34,6 @@ return {
         typescriptreact = { "prettier" },
         vue = { "prettier" },
         yaml = { "prettier" },
-        java = { "google_java_format" },
-        lua = { "stylua" },
-        python = { "isort", "black" },
-        rust = { "rustfmt" },
-        c = { "clang_format" },
-        cpp = { "clang_format" },
-        cs = { "clang_format" },
-        cuda = { "clang_format" },
-        proto = { "clang_format" },
-        arduino = { "clang_format" },
       },
       -- Customize formatters
       formatters = {
@@ -198,61 +199,4 @@ return {
       },
     },
   },
-
-  -- {
-  --   "nvimtools/none-ls.nvim",
-  --   opts = {
-  --     ensure_installed = {
-  --       "prettier",
-  --       "black",
-  --       "stylua",
-  --       "rustfmt",
-  --       "google_gava_format",
-  --       "xmllint",
-  --     },
-  --   },
-  --   config = function()
-  --     local null_ls = require("null-ls")
-  --     local formatting = null_ls.builtins.formatting
-  --     local diagnostics = null_ls.builtins.diagnostics
-  --     null_ls.setup({
-  --       debug = false,
-  --       sources = {
-  --         formatting.prettier.with({
-  --           extra_filetypes = { "toml" },
-  --           extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" },
-  --           filetypes = {
-  --             "css",
-  --             "graphql",
-  --             "handlebars",
-  --             "html",
-  --             "javascript",
-  --             "javascriptreact",
-  --             "json",
-  --             "jsonc",
-  --             "less",
-  --             "scss",
-  --             "svelte",
-  --             "typescript",
-  --             "typescriptreact",
-  --             "vue",
-  --             "yaml",
-  --           },
-  --         }),
-  --         formatting.clang_format.with({
-  --           filetypes = { "c", "cpp", "cs", "java", "cuda", "proto", "arduino" },
-  --         }),
-  --         formatting.black.with({ extra_args = { "--fast" } }),
-  --         formatting.stylua,
-  --         formatting.rustfmt,
-  --         formatting.google_java_format,
-  --         diagnostics.flake8.with({
-  --           args = { "--max-line-length", "88", "--format", "default", "--stdin-display-name", "$FILENAME", "-" },
-  --         }),
-  --         diagnostics.tidy,
-  --         formatting.xmllint,
-  --       },
-  --     })
-  --   end,
-  -- },
 }
